f3c3885c9d0b61276bc0aa62c29feaca1b0315d2
ghstx9 <66814310+ghstx9@users.noreply.github.com>
2025-04-29 09:52:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const body = document.body;
 toggleInput.addEventListener('change', () => {
   body.classList.toggle('dark-mode');
 });
+
+// localStorage check for saved theme
+if (localStorage.getItem('theme') === 'dark') {
+  body.classList.add('dark-mode');
+  toggleInput.checked = true; // Dark
+} else {
+  body.classList.remove('dark-mode');
+  toggleInput.checked = false; // Light
+}
+
+// Toggle when interacted with the switch
+toggleInput.addEventListener('change', () => {
+  if (toggleInput.checked) {
+    body.classList.add('dark-mode');
+    localStorage.setItem('theme', 'dark'); // Dark
+  } else {
+    body.classList.remove('dark-mode');
+    localStorage.setItem('theme', 'light'); // Light
+  }
+});
